6caa6ef4ccbac6631004671203c19a065302a860
Yu-Wei, Chen <210509fssh@gmail.com>
2020-06-19 22:24:38 +0800

    backup and still WIP

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ import utility
 
 os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 dataPath = "./kaggle/input"
-
+training_data_path = "./kaggle/training data"
+test_data_path = "./kaggle/test data"
 dt = pd.read_csv(dataPath + "/sales_train_validation.csv")
 dt.head(3)
 # Reduce memory usage and compare with the previous one to be sure
 dt = utility.downcast_dtypes(dt)
-dt, daysBeforeEventTest, event_typeTest, week_dayTest = utility.preprocessing(dt)
-print(dt.columns)
+# prepare
+utility.preprocessing(dt, startDay=350, finishDay=1913, savePath=training_data_path)
+# print(dt.columns)
 
-X_train, y_train = utility.prepare_training_data(dt)
+X_train, y_train = utility.prepare_training_data(data_path=training_data_path)
 model = utility.create_model(X_train.shape)
 
 # hyper parameter
@@ -27,7 +29,12 @@ checkPoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=2, save_best_
 # fit
 model.fit(X_train, y_train, epochs=epoch, batch_size=batch_size,
           validation_split=validation_spilt, verbose=1, callbacks=[early_stopping, checkPoint])
+
 # predict
+#prepare test data
+utility.preprocessing(dt, startDay=1913, finishDay=1941, savePath=test_data_path)
 model.load_weights("weights_best.hdf5")
-predictions = utility.predict(model, dt, daysBeforeEventTest, event_typeTest, week_dayTest)
+
+## TODo predict
+predictions = utility.predict(model, startDay=1913, data_path=test_data_path, training_dataset=training_data_path)
 utility.to_submission(predictions=predictions)
